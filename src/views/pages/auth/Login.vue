07d35d7f22ffce559f8ec8bab436b000a9ac63ec
shakib53626 <diu.shakib@gmail.com>
2024-02-19 12:32:31 +0600

    Delete All Custom Design

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -3,57 +3,8 @@
 </script>
 
 <template>
-    <div class="authentication-bg min-vh-100">
-        <div class="bg-overlay"></div>
-        <div class="container">
-            <div class="d-flex flex-column min-vh-100 px-3 pt-4">
-                <div class="row justify-content-center my-auto">
-                    <div class="col-md-8 col-lg-6 col-xl-5">
-
-                        <div class="card">
-                            <div class="card-body p-4"> 
-                                <div class="text-center mt-2">
-                                    <img src="@/assets/images/logo.webp" width="250" alt="">
-                                </div>
-                                <div class="p-2 mt-4">
-                                    <form action="index.html">
-    
-                                        <div class="mb-3">
-                                            <label class="form-label" for="username">Username</label>
-                                            <input type="text" class="form-control" id="username" placeholder="Enter username">
-                                        </div>
-            
-                                        <div class="mb-3">
-                                            <div class="float-end">
-                                                <a href="auth-recoverpw.html" class="text-muted">Forgot password?</a>
-                                            </div>
-                                            <label class="form-label" for="userpassword">Password</label>
-                                            <input type="password" class="form-control" id="userpassword" placeholder="Enter password">
-                                        </div>
-            
-                                        <div class="form-check">
-                                            <input type="checkbox" class="form-check-input" id="auth-remember-check">
-                                            <label class="form-check-label" for="auth-remember-check">Remember me</label>
-                                        </div>
-                                    
-                                        <div class="mt-3 text-end">
-                                            <button class="btn btn-primary w-sm waves-effect waves-light" type="submit">Log In</button>
-                                        </div>
-
-                                        <div class="mt-4 text-center">
-                                            <p class="mb-0">Don't have an account ? <a href="auth-register.html" class="fw-medium text-primary"> Signup now </a> </p>
-                                        </div>
-                                    </form>
-                                </div>
-        
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-        </div>
+    <div>
+        <h1>This is For Login View</h1>
     </div>
 </template>
 
