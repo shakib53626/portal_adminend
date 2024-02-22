6dbd28ac35380e06787ab6bb3de228dc3cc55757
shakib53626 <diu.shakib@gmail.com>
2024-02-22 20:46:02 +0600

    Header, footer and body part done, Logout done

--- src/views/common/Layout.vue
+++ src/views/common/Layout.vue
@@ -1,11 +1,18 @@
 <script setup>
+import { useThemeSetting } from '@/stores'
+import Header from '@/views/common/Header.vue'
+import Sidebar from '@/views/common/Sidebar.vue'
 
+const theme = useThemeSetting();
 </script>
 
 <template>
     <div>
         
-        <div class="content-wrapper">
+        <Header/>
+        <Sidebar/>
+
+        <div class="content-wrapper" :class="{'is-collops' : theme.isCollops}">
             <slot/>
         </div>
 
@@ -13,5 +20,13 @@
 </template>
 
 <style scoped>
-
+.content-wrapper{
+    margin-left: 270px;
+    margin-top: 60px;
+    transition: 0.5s;
+}
+.is-collops{
+    margin-left: 70px;
+    transition: 0.5s;
+}
 </style>
---
+++ src/views/common/Sidebar.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { useThemeSetting } from '@/stores'
+const theme = useThemeSetting();
+</script>
+
+<template>
+    <div class="sidebar" :class="{'is-collops-sidebar' : theme.isCollops}">
+        <div class="sidebar-logo" v-if="theme.isCollops"><img src="@/assets/images/logo/light-favicon.png" width="32" alt=""></div>
+        <div class="sidebar-logo" v-else><img src="@/assets/images/logo/logo.png" alt=""></div>
+    </div>
+</template>
+
+
+<style scoped>
+.sidebar{
+    width: 260px;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: #1C5990;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 3px 8px;
+    transition: 0.5s;
+}
+.is-collops-sidebar{
+    width: 60px;
+    transition: 0.5s;
+}
+.sidebar-logo{
+    padding: 10px 15px;
+}
+.sidebar-logo img{
+    max-width: 100%;
+}
+</style>
